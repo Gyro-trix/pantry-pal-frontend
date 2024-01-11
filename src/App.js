7135f819a3dccc7fc684a07da118e9036a5b3b07
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-11 14:01:53 -0330

    Experiment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ const count = 1;
 const mcolor = 'powderblue';
 
 function Flexi() {
-  return <button className="square">{count}</button>;
+  return (
+  <div className="card" style={{width: '25%'}}>
+  <img src="..." className="card-img-top" alt="..."></img>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+)
 }
 
 function App() {
@@ -50,26 +59,11 @@ function App() {
         </nav>
       </header>
       <br></br>
-      <div className="container-fluid mt-5 p-3" style={{ background: {mcolor} }}>
-        <div className="d-flex flex-column mb-3">
-          <div className="p-2 justify-content-end" >Flex item 1</div>
-          <div className="p-2">Flex item 2</div>
-          <div className="p-2">Flex item 3</div>
-        </div>
-        <div className="d-flex flex-column-reverse">
-          <div className="p-2">Flex item 1</div>
-          <div className="p-2">Flex item 2</div>
-          <div className="p-2">Flex item 3</div>
-        </div>
-        <div className="d-flex justify-content-start">...</div>
-        <div className="d-flex justify-content-end">...</div>
-        <div className="d-flex justify-content-center">...</div>
-        <div className="d-flex justify-content-between">...</div>
-        <div className="d-flex justify-content-around">...</div>
-        <div className="d-flex justify-content-evenly">...</div>
+      <div className="container-fluid mt-5 p-3" style={{ background: { mcolor } }}>
+        <Flexi /><Flexi />
 
       </div>
-      <Flexi /><Flexi />
+
     </div>
   );
 }

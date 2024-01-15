600184a4387844eb95713622c3041e09d2c3828c
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-14 22:38:18 -0330

    Clean up practise Code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-const nm = "Pantry Pal";
-const count = 1;
-const mcolor = 'powderblue';
-const title = "Pantry Pal";
 
 function App() {
   return (

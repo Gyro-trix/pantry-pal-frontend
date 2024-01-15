366455136044a00ad49f83885f7aef25949bf735
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-15 14:25:10 -0330

    Working with router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import React, { useState } from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
@@ -8,8 +9,9 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path = "/" element = {<Home/>}>
-        <Route index element={<Home/>}/>
+      <Route path = "/" element = {<Login/>}>
+        <Route index element={<Login/>}/>
+        <Route path = "Home" element={<Home/>}/>
         <Route path = "Login" element={<Login/>}/>
         <Route path = "Register" element={<Register/>}/>
       </Route>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,40 @@
-function Login(){
-return(
-    <div></div>
-);
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Home from "./Home";
+
+function Login() {
+    const name = useRef()
+    const email = useRef()
+    const password = useRef()
+
+    const navigate = useNavigate()
+
+const handleClick = () =>{
+    
+    if(name.current.value&&email.current.value&&password.current.value){
+       localStorage.setItem("name",name.current.value)
+       localStorage.setItem("email",email.current.value)
+       localStorage.setItem("password",password.current.value)
+       navigate("/home")
+    }
+}
+
+    return (
+        <div className="container">
+            <div className="input_space">
+                <input placeholder="Name" type="text" ref={name} />
+            </div>
+            <div className="input_space">
+                <input placeholder="Email" type="text" ref={email} />
+            </div>
+            <div className="input_space">
+                <input placeholder="Password" type="password" ref={password} />
+            </div>
+            <button onClick = {handleClick}>Create Account</button>
+            <button onClick = {handleClick}>Log In</button>
+
+        </div>
+    );
 }
 
 export default Login;

d8fffafe992b2bd926cd3f042cb764e6dccc6e3e
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-17 19:54:37 -0330

    Small update to logging in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,16 +7,20 @@ function Login() {
     const allUserDataStr = localStorage.getItem("ALL_USERS")
     const allUserArray = JSON.parse("[" + allUserDataStr + "]")
     const navigate = useNavigate()
+    const attemptingUser = {id: " ", username: " ", email: " " ,password: " " }
+    localStorage.setItem("CUR_USER"," ")
 
     function logIn() {
-        const logIn = { username: name.current.value, password: password.current.value }
+        
         //Checks if both fields have a value
         if (name.current.value && password.current.value) {
+            attemptingUser.username = name.current.value
+            attemptingUser.password = password.current.value
             //Check for user in local storage
-            if (ValidateUser(logIn)) {
+            if (ValidateUser()) {
                 alert("Invalid")
             } else {
-                localStorage.setItem("CUR_USER", JSON.stringify(logIn))
+                localStorage.setItem("CUR_USER", JSON.stringify(attemptingUser))
                 navigate("/home")
             }
 
@@ -24,35 +28,37 @@ function Login() {
     }
 
 
-function ValidateUser(userLoggingIn) {
-    let result
-    for (let i = 0; i < allUserArray.length; i++) {
-        if (allUserArray[i].username === userLoggingIn.username && allUserArray[i].password === userLoggingIn.password) {
-            result = false
-            return result
-        } else {
-            result = true
+    function ValidateUser(u) {
+        let result
+        for (let i = 0; i < allUserArray.length; i++) {
+            if (allUserArray[i].username === attemptingUser.username && allUserArray[i].password === attemptingUser.password) {
+                attemptingUser.id = allUserArray[i].id
+                attemptingUser.email = allUserArray[i].email
+                result = false
+                return result
+            } else {
+                result = true
+            }
         }
+        return result
     }
-    return result
-}
-function goRegister() {
-    navigate("/register")
-}
+    function goRegister() {
+        navigate("/register")
+    }
 
-return (
-    <div className="container">
-        <div className="input_space">
-            <input placeholder="Name" type="text" ref={name} />
-        </div>
-        <div className="input_space">
-            <input placeholder="Password" type="password" ref={password} />
-        </div>
-        <button onClick={goRegister}>Register</button>
-        <button onClick={logIn}>Log In</button>
+    return (
+        <div className="container">
+            <div className="input_space">
+                <input placeholder="Name" type="text" ref={name} />
+            </div>
+            <div className="input_space">
+                <input placeholder="Password" type="password" ref={password} />
+            </div>
+            <button onClick={goRegister}>Register</button>
+            <button onClick={logIn}>Log In</button>
 
-    </div>
-);
+        </div>
+    );
 }
 
 export default Login;

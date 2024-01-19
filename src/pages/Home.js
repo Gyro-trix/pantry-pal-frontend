b48f409ad43226c23d7d92592f5ffcfe9e61871a
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-19 13:37:42 -0330

    Added redirect, shown by coach

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function checkUserLogin(currentUser,navigate){
+  if(currentUser === null || currentUser.trim() ===""){
+    navigate("/login")
+  }
+}
+
 function Home() {
+  const currentUser = localStorage.getItem("CUR_USER")
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkUserLogin(currentUser,navigate)
+  })
+  
   return (
     <div className="card" style={{ width: "50%" }}>
       HOME PAGE

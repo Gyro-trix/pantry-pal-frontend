ee4d7812ba0dfc44111fcf4e903d0407bf94ff0c
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-22 15:08:27 -0330

    Added non functional Nav Bar and Log out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,58 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function checkUserLogin(currentUser,navigate){
-  if(currentUser === null || currentUser.trim() ===""){
+function checkUserLogin(currentUser, navigate) {
+  if (currentUser === null || currentUser.trim() === "") {
     navigate("/login")
   }
 }
+
+
 
 function Home() {
   const currentUser = localStorage.getItem("CUR_USER")
   const navigate = useNavigate();
 
   useEffect(() => {
-    checkUserLogin(currentUser,navigate)
+    checkUserLogin(currentUser, navigate)
   })
-  
+
+  function logOut() {
+
+
+    localStorage.setItem("CUR_USER", "")
+    navigate("\login")
+  }
+
   return (
-    <div className="card" style={{ width: "50%" }}>
-      HOME PAGE
-    </div>
-  );
+
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Pantry Pal</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link " aria-disabled="true" onClick={logOut}>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+  )
 }
 
 

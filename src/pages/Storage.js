95d6e82a4b3b8010254f25b7e22d6ff07adeddbf
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-24 20:04:29 -0330

    Now checks storage for duplicates before add

--- src/pages/Storage.js
+++ src/pages/Storage.js
@@ -11,51 +11,56 @@ function Storage() {
     const storagename = useRef()
     const storagetype = useRef()
     const storagelocation = useRef()
-
     const navigate = useNavigate()
-
     const currentUser = localStorage.getItem("CUR_USER")
-
     const allStorageDataStr = localStorage.getItem("ALL_STORAGES")
     const allStorageData = JSON.parse(allStorageDataStr)
-
     //Checks for User
     useEffect(() => {
         checkUserLogin(currentUser, navigate)
     })
-
+    //Gets form information and checks before saving to local storage
     function addStorage() {
         if (storagename && storagetype && storagelocation) {
             const sname = storagename.current.value
             const stype = storagetype.current.value
             const sloc = storagelocation.current.value
-
             const newStorage = { name: sname, type: stype, location: sloc, items: [] }
+            if(storageExists(allStorageData,newStorage) === false){
             if (allStorageDataStr === null ) {
                 console.log(newStorage)
                 localStorage.setItem("ALL_STORAGES", JSON.stringify([newStorage]))
             } else {
                 saveStorage(allStorageData, newStorage)
-                
             }
             window.location.reload(true);
         }
+        }
     }
-
+    //Save 'storage' to local storage
     function saveStorage(allStorage, newStorage) {
         let temparr = [...allStorage, newStorage]
         allStorage = temparr
         localStorage.setItem("ALL_STORAGES", JSON.stringify(allStorage))
     }
 
+    function storageExists(allStorage,storageToAdd){
+        for (let i = 0; i < allStorage.length; i++){
+            if (allStorage[i].name === storageToAdd.name){
+                return true
+            }
+        }
+        return false
+    }
 
-    //function addItem() {
-    //    if (item.current.value) {
-    //        setItemList([...itemlist, item.current.value])
-    //        console.log(itemlist)
-    //    }
-    //}
-
+    /*Old add item function, saving for possible future reference
+    function addItem() {
+       if (item.current.value) {
+           setItemList([...itemlist, item.current.value])
+            console.log(itemlist)
+        }
+    }
+*/
     return (
         <div className="container">
             <div className="input_space">

10a9b956048b141e1a803721bbc1a604ddf7286e
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-24 12:59:46 -0330

    Working on Storage

--- src/pages/Storage.js
+++ src/pages/Storage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function checkUserLogin(currentUser, navigate) {
@@ -10,17 +10,30 @@ function checkUserLogin(currentUser, navigate) {
 function Storage() {
     const storagename = useRef()
     const storagetype = useRef()
+    //const itementered = useRef()
+    const [item, setItem] = useState('')
+    const [itemlist, setItemList] = useState([])
     const navigate = useNavigate();
     const currentUser = localStorage.getItem("CUR_USER")
     const allStorageData = [localStorage.getItem("ALL_STORAGES")]
+
+    //Checks for User
     useEffect(() => {
         checkUserLogin(currentUser, navigate)
     })
-    
-    function addStorage(){
 
+    function addStorage() {
+        console.log(itemlist)
+        console.log(allStorageData)
     }
-    
+
+    function addItem() {
+        if (item.current.value) {
+            setItemList([...itemlist, item.current.value])
+            console.log(itemlist)
+        }
+    }
+
     return (
         <div className="container">
             <div className="input_space">
@@ -29,7 +42,16 @@ function Storage() {
             <div className="input_space">
                 <input placeholder="Storage Type" type="text" ref={storagetype} />
             </div>
-            <button >Add Storage</button>
+            <br></br>
+            <div className="input_space">
+                <input value={item} onChange ={setItem(e => e.target.value)} />
+            </div>
+            
+           
+
+            <button onClick={addItem}>Add Item</button>
+            <br></br>
+            <button onClick={addStorage}>Add Storage</button>
         </div>
     )
 }

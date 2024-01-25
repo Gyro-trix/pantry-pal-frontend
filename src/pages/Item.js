0f7cb6bd89915f70a81dc365b99f26190f2e05a6
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-25 18:34:08 -0330

    More

---
+++ src/pages/Item.js
@@ -0,0 +1,20 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+
+function Item(){
+
+
+
+return(
+<div>
+    <NavBar />
+    <div className = "container">
+        
+    </div>
+</div>
+)
+
+}
+
+export default Item;

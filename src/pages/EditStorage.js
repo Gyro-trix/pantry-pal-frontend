48c142c453cb442c995967ec693b9dfe8f6030e5
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-27 16:03:39 -0330

    Need to fix, match does not do exactly

--- src/pages/EditStorage.js
+++ src/pages/EditStorage.js
@@ -16,12 +16,9 @@ function EditStorage() {
     const allStorageDataStr = localStorage.getItem("ALL_STORAGES")
     const allStorageData = JSON.parse(allStorageDataStr)
     const currentStorageData = JSON.parse(localStorage.getItem("CUR_STORAGE"))
-
-    //console.log("Current STorage Data", currentStorageData, currentStorageData.location)
-    
-    const [name, setName] = useState(currentStorageData && currentStorageData.name? currentStorageData.name : "")
-    const [type, setType] = useState(currentStorageData && currentStorageData.type? currentStorageData.type : "")
-    const [location, setLocation] = useState(currentStorageData && currentStorageData.location? currentStorageData.location : "")
+    const [name, setName] = useState(currentStorageData && currentStorageData.name ? currentStorageData.name : "")
+    const [type, setType] = useState(currentStorageData && currentStorageData.type ? currentStorageData.type : "")
+    const [location, setLocation] = useState(currentStorageData && currentStorageData.location ? currentStorageData.location : "")
     const [filteredStorage, setFilteredStorage] = useState(allStorageData)
 
     //Checks for User
@@ -29,29 +26,27 @@ function EditStorage() {
         checkUserLogin(currentUser, navigate)
     })
 
-function checkStorage(storage){
-    console.log(JSON.stringify(storage))
-    console.log(JSON.stringify(currentStorageData))
-    
-    return JSON.stringify(storage) != JSON.stringify(currentStorageData)
-}
+    function saveStorage(allStorage, newStorage) {
+        let temparr = [...allStorage, newStorage]
+        allStorage = temparr
+        //localStorage.setItem("ALL_STORAGES", JSON.stringify(allStorage))
+    }
 
-    function editStorage(){
-        
-        
+    function editStorage() {
         // Filter removes storage we wish to edit
-        console.log("Before: ",allStorageData,name)
-        const filtered = allStorageData.filter(storage => !storage.name.includes(name))
+        const filtered = allStorageData.filter(storage => !storage.name.match("/"+ name + "/g"))
         setFilteredStorage(filtered)
-        console.log("After: ",filteredStorage)
-        
+        //Set values in form as the replacements     
         setName(storagename.current.value)
         setType(storagetype.current.value)
         setLocation(storagelocation.current.value)
-
-        console.log(storagename.current.value,name)
-        console.log(storagetype.current.value,type)
-        console.log(storagelocation.current.value,location)
+        //console.log(storagename.current.value, name)
+        //console.log(storagetype.current.value, type)
+        //console.log(storagelocation.current.value, location)
+        //console.log(currentStorageData.items)
+        //const temp ={name: name,type: type,location: location,items: currentStorageData.items}
+        //saveStorage(filteredStorage,temp)
+        console.log(filtered)
 
         // Pull all Edit Storage
 
@@ -77,14 +72,14 @@ function checkStorage(storage){
         <div>
             <div className="container">
                 <div className="input_space">
-                    <input placeholder="Storage Name" type="text" defaultValue={name} ref = {storagename}/>
+                    <input placeholder="Storage Name" type="text" defaultValue={name} ref={storagename} />
                 </div>
                 <div className="input_space">
-                    <input placeholder="Storage Type" type="text" defaultValue={type} ref = {storagetype}/>
+                    <input placeholder="Storage Type" type="text" defaultValue={type} ref={storagetype} />
                 </div>
                 <br></br>
                 <div className="input_space">
-                    <input placeholder="Location" type="text" defaultValue={location} ref = {storagelocation} />
+                    <input placeholder="Location" type="text" defaultValue={location} ref={storagelocation} />
                 </div>
                 <button onClick={editStorage}>Edit Storage</button>
             </div>

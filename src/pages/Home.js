86d8d4449821a50b212c04ea0e8ff801a5f28b3d
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-29 18:40:02 -0330

    Basic delete confirmation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,49 +9,44 @@ import { useNavigate } from "react-router-dom";
 //}
 
 function Home() {
-  //const currentUser = localStorage.getItem("CUR_USER")
 
   const allStorageDataStr = localStorage.getItem("ALL_STORAGES")
   const allStorageData = JSON.parse(allStorageDataStr)
 
   const navigate = useNavigate();
 
- // useEffect(() => {
- //   checkUserLogin(currentUser, navigate)
- // })
-
-function openEditStoragePage(singleStorageData){
-  localStorage.setItem("CUR_STORAGE",JSON.stringify(singleStorageData))
-  navigate("/EditStorage")
-}
-//Does delete, no comfirmation
-function deleteStorage(allStorage,singleStorageData){
-  localStorage.setItem("CUR_STORAGE",JSON.stringify(singleStorageData))
-  console.log(singleStorageData.name)
-  allStorage = allStorage.filter(storage => !storage.name.match(new RegExp('^' + singleStorageData.name + '$')))
-  localStorage.setItem("ALL_STORAGES",JSON.stringify(allStorage))
-  window.location.reload()
-}
+  function openEditStoragePage(singleStorageData) {
+    localStorage.setItem("CUR_STORAGE", JSON.stringify(singleStorageData))
+    navigate("/EditStorage")
+  }
+  //Does delete, no comfirmation
+  function deleteStorage(allStorage, singleStorageData) {
+    localStorage.setItem("CUR_STORAGE", JSON.stringify(singleStorageData))
+    console.log(singleStorageData.name)
+    allStorage = allStorage.filter(storage => !storage.name.match(new RegExp('^' + singleStorageData.name + '$')))
+    localStorage.setItem("ALL_STORAGES", JSON.stringify(allStorage))
+    window.location.reload()
+  }
 
   function displayStorage() {
-    if ((allStorageDataStr === null) === false){
-    return allStorageData.map((singleStorageData) => {
-      
-      return (
-        <div key = {singleStorageData.name} className="card" style={{marginTop: 10}}>
-          <div className = "card-body">
-            <h5 className = "card-title">{singleStorageData.name}</h5>
-            <p className = "card-text">{singleStorageData.type} & {singleStorageData.location}</p>
-            <a href="#" className="btn btn-primary" style={{marginRight: 15}} onClick={()=>openEditStoragePage(singleStorageData)}>Edit Storage</a>
-            <a href="#" className="btn btn-primary" onClick={()=>deleteStorage(allStorageData,singleStorageData)} >Delete Storage</a>
+    if ((allStorageDataStr === null) === false) {
+      return allStorageData.map((singleStorageData) => {
+
+        return (
+          <div key={singleStorageData.name} className="card" style={{ marginTop: 10 }}>
+            <div className="card-body">
+              <h5 className="card-title">{singleStorageData.name}</h5>
+              <p className="card-text">{singleStorageData.type} & {singleStorageData.location}</p>
+              <a href="#" className="btn btn-primary" style={{ marginRight: 15 }} onClick={() => openEditStoragePage(singleStorageData)}>Edit Storage</a>
+              <a href="#" className="btn btn-primary" onClick={() => {if(window.confirm('Delete the item?')){deleteStorage(allStorageData, singleStorageData)}}} >Delete Storage</a>
             </div>
           </div>
-      )
-    })
-  }
+        )
+      })
+    }
   }
   return (
-    <div style={{marginTop: 15, marginBottom: 15}}>
+    <div style={{ marginTop: 15, marginBottom: 15 }}>
       {displayStorage()}
     </div>
   )

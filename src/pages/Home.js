404037e82b134bcaaef6fefb4e89763087bddf4f
Matthew Garrett <m.garrett@hotmail.ca>
2024-01-29 18:11:02 -0330

    Delete Storage, needs confirmation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,6 +24,14 @@ function openEditStoragePage(singleStorageData){
   localStorage.setItem("CUR_STORAGE",JSON.stringify(singleStorageData))
   navigate("/EditStorage")
 }
+//Does delete, no comfirmation
+function deleteStorage(allStorage,singleStorageData){
+  localStorage.setItem("CUR_STORAGE",JSON.stringify(singleStorageData))
+  console.log(singleStorageData.name)
+  allStorage = allStorage.filter(storage => !storage.name.match(new RegExp('^' + singleStorageData.name + '$')))
+  localStorage.setItem("ALL_STORAGES",JSON.stringify(allStorage))
+  window.location.reload()
+}
 
   function displayStorage() {
     if ((allStorageDataStr === null) === false){
@@ -35,7 +43,7 @@ function openEditStoragePage(singleStorageData){
             <h5 className = "card-title">{singleStorageData.name}</h5>
             <p className = "card-text">{singleStorageData.type} & {singleStorageData.location}</p>
             <a href="#" className="btn btn-primary" style={{marginRight: 15}} onClick={()=>openEditStoragePage(singleStorageData)}>Edit Storage</a>
-            <a href="#" className="btn btn-primary">Delete Storage</a>
+            <a href="#" className="btn btn-primary" onClick={()=>deleteStorage(allStorageData,singleStorageData)} >Delete Storage</a>
             </div>
           </div>
       )

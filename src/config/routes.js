7575e95551d259362994cd76f6b96527a5de66d0
Matthew Garrett <m.garrett@hotmail.ca>
2024-02-09 17:05:24 -0330

    Starting Cleanup

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,3 +1,9 @@
 module.exports = {
-    SIGN_IN : "/login"
+    SIGN_IN : "/login",
+    NOTIFICATION : " ",
+    SETTINGS : " ",
+    EDIT_STORAGE : " ",
+    CREATE_STORAGE : "",
+    HOME: "",
+    REGISTER: "",
 }

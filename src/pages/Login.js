f630d112438b43f603c1ed90e1043a9212ebc385
Matthew Garrett <m.garrett@hotmail.ca>
2024-02-20 09:01:45 -0330

    disabling admin account creation at login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,15 +6,16 @@ import { ALL_USERS, CUR_USER } from "../config/localStorage"
 
 
 function Login() {
-    //Insures current user variable is entered if the user goes back to login
+    //Treats navigating to Log In as logging out
     localStorage.setItem(CUR_USER, "")
     const navigate = useNavigate()
-    const allUserDataStr = [localStorage.getItem(ALL_USERS)]
+
+    //const allUserDataStr = [localStorage.getItem(ALL_USERS)]
     const [attemptingUser, setAttemptingUser] = useState({ id: " ", username: " ", email: " ", password: " ", notify: " ", itemlimit: " ", expirylimit: " " })
     //One way to deal with ALL_USER starting with null, could also check for null later and adjust
-    if (allUserDataStr[0] === null) {
+    /*if (allUserDataStr[0] === null) {
         localStorage.setItem(ALL_USERS, JSON.stringify([{ id: "TrueAdmin", username: "Admin", email: "Admin" }]))
-    }
+    }*/
     const handleChange = e => {
         setAttemptingUser((prev) => ({
             ...prev,
@@ -23,7 +24,7 @@ function Login() {
     }
 
     function goRegister() {
-        localStorage.setItem(CUR_USER, JSON.stringify({ id: "TrueAdmin", username: "Admin", email: "Admin", password: "admin" }))
+       // localStorage.setItem(CUR_USER, JSON.stringify({ id: "TrueAdmin", username: "Admin", email: "Admin", password: "admin" }))
         navigate(REGISTER)
     }
 

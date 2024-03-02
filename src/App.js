236983345c0d0a9ba52f64a19d5ab80ff5b5a29a
Matthew Garrett <m.garrett@hotmail.ca>
2024-03-02 17:46:25 -0330

    Added toastify for failed login

--- src/App.js
+++ src/App.js
@@ -14,35 +14,37 @@ import ManageUsers from './pages/ManageUsers';
 import EditUser from './pages/EditUser'
 import CreateUser from './pages/CreateUser'
 import EditRecipe from './pages/EditRecipe'
-
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
-  
-  return (
-    <div style = {{background:"lightblue", height:850, minWidth:600}}>
-      
-      <BrowserRouter>
-      <NavBar />  
-      <div >
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/createStorage" element={<CreateStorage />} />
-        <Route path="/editStorage" element={<EditStorage />} />
-        <Route path="/userSettings" element={<UserSettings />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/createrecipes" element={<CreateRecipes />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/manageusers" element={<ManageUsers />} />
-        <Route path="/edituser" element={<EditUser />} />
-        <Route path="/createuser" element={<CreateUser />} />
-        <Route path="/editrecipe" element={<EditRecipe />} />
 
-      </Routes>
-      </div>
-    </BrowserRouter>
+  return (
+    <div style={{ background: "lightblue", height: 850, minWidth: 600 }}>
+
+      <BrowserRouter>
+        <NavBar />
+        <div >
+          <ToastContainer />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/createStorage" element={<CreateStorage />} />
+            <Route path="/editStorage" element={<EditStorage />} />
+            <Route path="/userSettings" element={<UserSettings />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/createrecipes" element={<CreateRecipes />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/manageusers" element={<ManageUsers />} />
+            <Route path="/edituser" element={<EditUser />} />
+            <Route path="/createuser" element={<CreateUser />} />
+            <Route path="/editrecipe" element={<EditRecipe />} />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }

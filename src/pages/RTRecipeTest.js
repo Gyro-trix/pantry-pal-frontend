44c7507b015e3174f9d85601d8e0938323cd549d
Matthew Garrett <m.garrett@hotmail.ca>
2024-03-05 20:28:16 -0330

    Testing Jodit editor

--- src/pages/RTRecipeTest.js
+++ src/pages/RTRecipeTest.js
@@ -1,13 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Quill from 'quill';
-import "quill/dist/quill.snow.css"
+import React, { useState, useEffect, useRef,useMemo } from 'react'
+import JoditEditor from 'jodit-react';
+
 
 function RTRecipeTest() {
-  return(
-    <div>
-      
-    </div>
-  )
+  const editor = useRef(null);
+	const [content, setContent] = useState('');
+
+	const config = useMemo(()=>
+		({uploader: { "insertImageAsBase64URI": true }}),
+		[]
+	);
+
+	return (
+		<JoditEditor
+			ref={editor}
+			value={content}
+			config={config}
+			tabIndex={1} // tabIndex of textarea
+			onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+			onChange={newContent => {}}
+		/>
+	)
 }
 
 /*Quill JS

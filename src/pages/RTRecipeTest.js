68c6a346afe0965dc5e48829eeb4fd3ccb5312ec
Matthew Garrett <m.garrett@hotmail.ca>
2024-03-05 15:14:01 -0330

    Reverting due to issues with a Rich Text Editor API

--- src/pages/RTRecipeTest.js
+++ src/pages/RTRecipeTest.js
@@ -1,9 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
-import Quill from 'quill';
-import "quill/dist/quill.core.css";
 
 function RTRecipeTest(){
 return(

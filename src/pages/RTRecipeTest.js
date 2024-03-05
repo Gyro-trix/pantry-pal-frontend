9d293e51231afd9a4e5c5fda00a43f6bba38ce6b
Matthew Garrett <m.garrett@hotmail.ca>
2024-03-05 15:10:44 -0330

    Revert "Movement based animations"
    This reverts commit db7431d55d1320e12eb5a619cbbddd1e98ef5821.

--- src/pages/RTRecipeTest.js
+++ src/pages/RTRecipeTest.js
@@ -5,28 +5,10 @@ import 'react-quill/dist/quill.snow.css';
 import Quill from 'quill';
 import "quill/dist/quill.core.css";
 
-
-
-function RTRecipeTest() {
-  const container = document.getElementById('editor')
-  const options = {
-    debug: 'info',
-    placeholder: 'Compose an epic...',
-    theme: 'snow'
-  }
-  const [quill,setQuill] = useState("")
-useEffect(()=>{
-setQuill(new Quill(container, options))
-},[container])
-
-  return (
-    <div id="no" style={{ background: "white" }}>
-      <div id='editor'></div>
-      <button >Save Recipe</button>
-
-    </div>
-  )
-
+function RTRecipeTest(){
+return(
+    <div></div>
+)
 }
 
 export default RTRecipeTest

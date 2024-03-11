af90755ee6ef31e6449aa98cb1455404da8f1779
Matthew Garrett <m.garrett@hotmail.ca>
2024-03-11 14:15:09 -0230

    env file and api call work

--- src/pages/AddItems.js
+++ src/pages/AddItems.js
@@ -6,7 +6,8 @@ import { CUR_ITEM_LIST } from "../config/localStorage";
 
 function AddItems(props) {
     const [startDate, setStartDate] = useState(new Date())
-
+    const [data,setData] = useState(null)
+    const [ingredient,setIngredient] = useState('chicken')
     const [item, setItem] = useState({
         quantity: 0,
         name: "",
@@ -20,6 +21,28 @@ function AddItems(props) {
         localStorage.setItem(CUR_ITEM_LIST, JSON.stringify(itemlist))
     }, [itemlist])
 
+    const handleSearch = async () => {
+
+        const apiKey = process.env.REACT_APP_CALORIE_NINJAS_KEY
+        const search = 'https://api.calorieninjas.com/v1/nutrition?query=' + ingredient
+
+        try {
+            const response = await fetch(search, {
+                method: 'GET',
+                headers: {
+                    'X-Api-Key': apiKey
+                }
+            })
+            if (!response.ok) {
+                throw new Error('Response was not okay')
+            }
+
+            setData(await response.json())
+        } catch (error) {
+            console.error('Error', error)
+        }
+
+    }
 
     const handleChange = e => {
         setItem((prev) => ({
@@ -31,26 +54,26 @@ function AddItems(props) {
     return (
         <div>
             <div className="container">
-                <div className="container" style={{marginTop:16 }} >
+                <div className="container" style={{ marginTop: 16 }} >
                     {displayItems()}
                 </div>
                 <div className="input-group" style={{ marginTop: 10 }}>
                     <input className="form-control"
-                        style={{ }}
+                        style={{}}
                         type="number"
                         onChange={handleChange}
                         name="quantity"
                         placeholder="Quantity"
                     ></input>
                     <input className="form-control"
-                        style={{  }}
+                        style={{}}
                         type="text"
                         onChange={handleChange}
                         name="name"
                         placeholder="Name"
                     ></input>
                     <input className="form-control"
-                        style={{  }}
+                        style={{}}
                         type="text"
                         onChange={handleChange}
                         name="size"
@@ -58,7 +81,7 @@ function AddItems(props) {
                     ></input>
                     <DatePicker
                         className="form-control"
-                        style = {{borderRadius: 0.0}}
+                        style={{ borderRadius: 0.0 }}
                         selected={startDate}
                         name="expiry"
                         onChange={(date) => {
@@ -68,10 +91,11 @@ function AddItems(props) {
                         }
                         }
                     />
-                    <button type="button" className="btn btn-primary" style={{ whiteSpace: "nowrap"}} onClick={() => addItem(item)}>Add Item</button>
-
+                    <button type="button" className="btn btn-primary" style={{ whiteSpace: "nowrap" }} onClick={() => addItem(item)}>Add Item</button>
+                    <button type="button" className="btn btn-primary" style={{ whiteSpace: "nowrap" }} onClick={() => handleSearch()}>Test</button>
                 </div>
             </div>
+            {JSON.stringify(data)} Hello There
         </div>
     )
 
